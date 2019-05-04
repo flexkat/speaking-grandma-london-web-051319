4dbac7c645d8741fbdbc51408624276d5b144bc9
Catherine Maritz <catherinemaritz1@gmail.com>
2019-05-04 14:32:12 +0100

    speaking grandma conditional lab

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(string)
+  if string == string.upcase
+    "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
